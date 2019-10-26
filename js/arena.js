d09d4919122d388e045bf913d6c98b6f0c103ebd
Muneera Bin Hotan <muneerabinhotan@gmail.com>
2019-10-26 19:25:59 +0300

    finished

---
+++ js/arena.js
@@ -0,0 +1,28 @@
+export default class Arena {
+    
+    constructor(name) {
+    this.name = name.charAt(0).toUpperCase() + name.slice(1)
+    this.gladiators = [];}
+
+
+    addGladiator(gladiator) {
+    if (this.gladiators.length <= 2){
+    this.gladiators.push(gladiator);}
+    
+    else {
+    return 'More than 2 Gladiators in the Arena!';}}
+    
+
+    fight() {
+    
+    if(this.gladiators[0].weapon == "Trident" && this.gladiators[1].weapon == "Spear" || this.gladiators[0].weapon == "Spear" && this.gladiators[1].weapon == "Club" || this.gladiators[0].weapon == "Club" && this.gladiators[1].weapon == "Trident"){
+        return `${this.gladiators[0].name} Won!`
+
+    }
+    else if(this.gladiators[1].weapon == "Trident" && this.gladiators[0].weapon == "Spear" || this.gladiators[1].weapon == "Spear" && this.gladiators[0].weapon == "Club" || this.gladiators[1].weapon == "Club" && this.gladiators[0].weapon == "Trident"){
+        return `${this.gladiators[1].name} Won!`
+
+    }
+    else{
+        return 'Both are eliminated'
+    }}}
